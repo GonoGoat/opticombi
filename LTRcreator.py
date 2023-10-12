c380ee79dadc220bcb9373a9bf5758e717b71984
MaxenceManteau <maxence.manteau@std.heh.be>
2023-10-12 10:34:02 +0200

    Correction creation fichier LTR

--- LTRcreator.py
+++ LTRcreator.py
@@ -1,17 +1,20 @@
-# Les valeurs à inclure dans le fichier LTR
+def create_ltr_file(name, solver, sequence, output_file):
+    # Calculer la longueur de la séquence pour le score
+    score = len(sequence)
+
+    # Ouvrir le fichier en mode écriture
+    with open(output_file, "w") as fichier:
+        fichier.write(f"Name: {name}\n")
+        fichier.write(f"Solver: {solver}\n")
+        fichier.write(f"Score: {score}\n")
+        fichier.write(f"\n{sequence}\n")
+
+    print(f"Le fichier {output_file} a été créé avec succès.")
+
+# Utilisation de la fonction avec des valeurs spécifiques
 name = "One block into the water"
 solver = "Yono-Yono"
-score = 160
 sequence = "RRUFFUULLUUURRDFDLLDDRFFFRRRDDDRRRUUULFDDDLLLUFFFUUUURRUUURRUUULLLDDLLDDDRFUUURRUURRRDDDLLDFDLLDDRFFFRRRDDRRRRRUUULLLLDFDLLDDRFFFRUUURRRRDFFDDDLLDDDRRRUULFFLLLL"
+output_file = "mon_fichier2.ltr"
 
-# Nom du fichier de sortie
-fichier_ltr = "mon_fichier2.ltr"
-
-# Ouvrir le fichier en mode écriture
-with open(fichier_ltr, "w") as fichier:
-    fichier.write(f"Name: {name}\n")
-    fichier.write(f"Solver: {solver}\n")
-    fichier.write(f"Score: {score}\n")
-    fichier.write(f"{sequence}\n")
-
-print(f"Le fichier {fichier_ltr} a été créé avec succès.")
+create_ltr_file(name, solver, sequence, output_file)
