ac82d6e5f9e499229970a43190a216c3f936c74a
GonoGoat <gauth.vrsgn@outlook.be>
2023-11-19 11:07:53 +0100

    Librairies SVG

--- Libs/sequenceToSVG.h
+++ Libs/sequenceToSVG.h
@@ -5,3 +5,12 @@
 // Dessin trajectoire
 #include "..\utils\Parseur.cpp"
 #include "..\utils\Detection_Start_End.cpp"
+#include <string>
+
+std::string getInitSVG ();
+std::string getRect();
+std::string getCircle();
+std::string getStart();
+std::string getEnd();
+std::string getSection();
+void drawSVG(std::string output_file);
